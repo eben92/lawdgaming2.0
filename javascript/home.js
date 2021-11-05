39ab7da49c4dcbd9defdfd4f088ad4792f80fc9a
EBEN <pappy.rhutmannjnrolege@gmail.com>
2021-11-05 09:16:13 +0000

    time update

--- javascript/home.js
+++ javascript/home.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Nov 1, 2021 00:00:00").getTime();
+var countDownDate = new Date("Dec 1, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
